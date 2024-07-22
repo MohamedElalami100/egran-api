b52b51c5ec234dd94338c984bd92c95563ca24cd
simo1111 <elalamimohamed2k17@gmail.com>
2024-07-22 14:53:52 +0100

    first commit

---
+++ src/main/java/com/egran/egran_api/EgranApiApplication.java
@@ -0,0 +1,13 @@
+package com.egran.egran_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgranApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgranApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egran/egran_api/EgranApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egran.egran_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgranApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
