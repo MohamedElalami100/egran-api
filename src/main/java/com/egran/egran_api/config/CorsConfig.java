c20e9307db9b21b74e69a791505375f93bd21be1
simo1111 <elalamimohamed2k17@gmail.com>
2024-10-11 02:24:06 +0100

    Update Cors policy

--- src/main/java/com/egran/egran_api/config/CorsConfig.java
+++ src/main/java/com/egran/egran_api/config/CorsConfig.java
@@ -1,30 +1,20 @@
 package com.egran.egran_api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
-        // Specify the domain of your frontend explicitly instead of "*"
-        config.addAllowedOrigin("https://main.d14g9pqq90b83n.amplifyapp.com/");  // Amplify domain
-
-        // Allow credentials if required (cookies, auth headers, etc.)
-        config.setAllowCredentials(true);
-
-        // Allow all headers and methods
+        config.addAllowedOrigin("*"); // Allow requests from all origins
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-
-        // Register the configuration for all paths
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
